be3bac943168ef1c1a487dcbf8758ffb698bb9f9
mohmdalfaha <mohmd1919@gmail.com>
2019-02-07 03:57:36 +0300

    refactor: Add middleware with few changes and updates to dependencies Here was an issue solved by updating package dependencies

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,17 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
+import { setAuthedUser } from '../actions/authedUser'
 
-export function handleInditialData () {
+const AUTHED_ID = 'mohmdalfaha'
+
+export function handleInitialData () {
   return (dispatch) => {
     return getInitialData()
       .then(({ users, tweets}) => {
-        dispatch((receiveUsers(users))
-        dispatch((receiveTweets(tweets))
+        dispatch(receiveUsers(users))
+        dispatch(receiveTweets(tweets))
+        dispatch(setAuthedUser(AUTHED_ID))
       })
   }
 }
